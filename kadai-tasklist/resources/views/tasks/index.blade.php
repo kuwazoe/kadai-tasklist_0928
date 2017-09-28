b0fbc2da55818ad7423f688a0debff6e5977c3a1
kuwazoe <hkuwazoe@ipcreate.co.jp>
2017-09-28 11:59:52 +0000

    kadai-tasklist

---
+++ kadai-tasklist/resources/views/tasks/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク一覧</h1>
+    
+    @if (count($tasks) > 0)
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}} : {{ $task->content }}</li>
+            @endforeach
+        </ul>
+    @endif
+    {!! Form::model(['route' => ['tasks.create'], 'method' => 'create']) !!}
+        {!! Form::submit('新規作成') !!}
+    {!! Form::close() !!}
+
+@endsection
